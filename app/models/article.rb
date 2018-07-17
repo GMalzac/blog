3a321b40cd9aab0e443b511ad746442f27f1f563
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-07-17 19:16:02 +0100

    text reduction methods included  in model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,28 @@ class Article < ApplicationRecord
                     length: { minimum: 5 }
   validates :subtitle, presence: true,
                     length: { minimum: 5 }
+
+  def short_title
+    if self.title.length > 30
+      self.title[0..30] + "..."
+    else
+      self.title
+    end
+  end
+
+  def short_subtitle
+    if self.subtitle.length > 75
+      self.subtitle[0..75] + "..."
+    else
+      self.subtitle
+    end
+  end
+
+  def short_text
+    if self.text.length > 480
+      self.text[0..480] + "..."
+    else
+      self.text
+    end
+  end
 end
